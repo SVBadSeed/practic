168cf2424320c7fada0842e1ee8b43532e6d4517
Masthead <alexeev.tolik2015@yandex.ru>
2022-10-30 19:52:28 +0300

    addPost js

--- src/assets/js/addPost.js
+++ src/assets/js/addPost.js
@@ -1,14 +1,19 @@
-import {addPost} from './constants/addPosts';
+import {addPost} from './constants/addPosts'
+
 
 let buttonSend = document.querySelector('.add-post__send')
 let posts = document.querySelector('.post__js1')
+let postTextarea = document.querySelector('.add-post__textarea')
 
 buttonSend.addEventListener('click', event => {
     event.preventDefault()
+    let localData = new Date().toLocaleString().slice(0, 10)
+    let text = postTextarea.value
+    postTextarea.value = ''
 
     let article = document.createElement('article')
     article.classList.add('post')
-    article.innerHTML = addPost(event)
+    article.innerHTML = addPost(localData, text)
 
     posts.prepend(article)
 })
--- src/assets/js/constants/addPosts.js
+++ src/assets/js/constants/addPosts.js
@@ -1,15 +1,15 @@
-export const addPost = () => {
+export const addPost = (date, text) => {
     return `
      <div class="post__content">
        <p class="post__description">
-          смерть и разложение
+          ${text}
        </p>
     </div>
     <div class="post__footer">
         <ul class="post__data">
             <li class="post__data-item">
                  <time datetime="2020-06-21">
-                   27.11.2020
+                 ${date}
                 </time>
             </li>
         </ul>
