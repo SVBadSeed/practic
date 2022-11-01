54a859e3222f9519d691cdfe892b03f69418cce0
Masthead <alexeev.tolik2015@yandex.ru>
2022-11-01 23:18:14 +0300

    js modal

--- src/assets/js/modal.js
+++ src/assets/js/modal.js
@@ -3,7 +3,6 @@ const body = document.body
 const modalClose = document.querySelectorAll('.modal__close')
 const modal = document.querySelectorAll('.modal')
 
-
 modalBtn.forEach(item => {
     item.addEventListener('click', event => {
         let $this = event.currentTarget
