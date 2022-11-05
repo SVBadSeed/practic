b99be0fade29b4c71314aea98f3ac7aa8ad46e50
Masthead <alexeev.tolik2015@yandex.ru>
2022-11-05 23:09:41 +0300

    works js

--- src/assets/js/work.js
+++ src/assets/js/work.js
@@ -1,34 +1,51 @@
-// import {getRandomWorkText, getContentWorkHtml} from './constants/works'
-//
-// let workContainer = document.querySelector('.work__container')
-// let text = getRandomWorkText()
-//
-// let workContent = [
-//     {
-//         image: '',
-//         title: 'altermono.com',
-//         text: text,
-//         tags: [
-//             {title: 'дизайн'},
-//             {title: 'создание сайта'},
-//             {title: 'SMM'}
-//         ],
-//     }
-// ]
-//
-//
-// workContent.forEach(item => {
-//     let article = document.createElement('article')
-//     article.classList.add('work')
-//     article.innerHTML = getContentWorkHtml(item)
-//
-//     console.log(article)
-//
-//     workContainer.append(article)
-// })
+import {getRandomWorkText, getContentWorkHtml} from './constants/works'
 
+let workContainer = document.querySelector('.work__container')
+let text = getRandomWorkText()
+let workContent = [
+    {
+        image: 'assets/images/work-preview.jpg',
+        title: 'altermono.com',
+        text: text,
+        tags: [
+            {title: 'дизайн'},
+            {title: 'создание сайта'},
+            {title: 'SMM'}
+        ],
+        link: 'Перейти на сайт'
+    }, {
+        image: 'assets/images/work-preview-2.jpg',
+        title: 'codedoco.com',
+        text: text,
+        tags: [
+            {title: 'дизайн'},
+            {title: 'создание сайта'},
+            {title: 'SMM'},
+            {title: 'дизайн'},
+            {title: 'создание сайта'},
+            {title: 'SMM'}
+        ],
+        link: 'Перейти на сайт'
+    }
+]
 
+workContent.forEach(item => {
+    let article = document.createElement('article')
+    article.className = 'work'
+    article.innerHTML = getContentWorkHtml(item)
 
+    let tagContainer = article.querySelector('.tags')
+
+    item.tags.forEach(tag => {
+        let tagsItem = document.createElement('li')
+        tagsItem.className = 'tags__item'
+        tagsItem.innerHTML = tag.title
+
+        tagContainer.appendChild(tagsItem)
+    })
+
+    workContainer.append(article)
+})
 
 
 
