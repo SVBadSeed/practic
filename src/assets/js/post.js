6a366eb1f5c17df154763bf07c74376725bf11db
Masthead <alexeev.tolik2015@yandex.ru>
2022-11-19 17:18:35 +0300

    JS post server

--- src/assets/js/post.js
+++ src/assets/js/post.js
@@ -9,8 +9,19 @@ const requestPostsURL = 'http://localhost:3100/posts'
 
 async function fetchPosts() {
     let response = await fetch(requestPostsURL, {method: 'GET'})
+
     return response.json()
 }
+
+fetchPosts().then(responseData => {
+    renderPosts(responseData)
+    let postAll = document.querySelectorAll('.post__js1 > .post')
+
+    renderClosePosts()
+    renderFilterPosts(postAll)
+    renderFilter2Posts(postAll)
+
+})
 
 function renderPosts(responseData) {
     postContent.push(...responseData)
@@ -31,44 +42,44 @@ function renderPosts(responseData) {
     })
 }
 
-fetchPosts().then(responseData => {
-    renderPosts(responseData)
-})
+function renderFilterPosts(postAll) {
+    let buttonFilter = document.querySelector('.add-post__send--red')
 
-
-let buttonFilter = document.querySelector('.add-post__send--red')
-let buttonFilter2 = document.querySelector('.add-post__send--orange')
-let postAll = document.querySelectorAll('.post__js1 > .post')
-let postButtonClose = document.querySelectorAll('.post__button-close')
-
-buttonFilter.addEventListener('click', event => {
-    event.preventDefault()
-
-    postAll.forEach(post => {
-        post.style.display = 'block'
-        let postCategory = post.querySelector('.post__category')
-        if (postCategory.textContent !== 'Создание сайтов') {
-            post.style.display = 'none'
-        }
+    buttonFilter.addEventListener('click', event => {
+        event.preventDefault()
+        postAll.forEach(post => {
+            post.style.display = 'block'
+            let postCategory = post.querySelector('.post__category')
+            if (postCategory.textContent !== 'Создание сайтов') {
+                post.style.display = 'none'
+            }
+        })
     })
+}
 
-})
+function renderFilter2Posts(postAll) {
+    let buttonFilter2 = document.querySelector('.add-post__send--orange')
 
-buttonFilter2.addEventListener('click', event => {
-    event.preventDefault()
+    buttonFilter2.addEventListener('click', event => {
+        event.preventDefault()
 
-    postAll.forEach(post => {
-        post.style.display = 'block'
-        let postCategory = post.querySelector('.post__category')
-        if (postCategory.textContent !== 'Заметка') {
-            post.style.display = 'none'
-        }
+        postAll.forEach(post => {
+            post.style.display = 'block'
+            let postCategory = post.querySelector('.post__category')
+            if (postCategory.textContent !== 'Заметка') {
+                post.style.display = 'none'
+            }
+        })
     })
-})
+}
 
-postButtonClose.forEach(item => {
-    item.addEventListener('click', event => {
-        let parent = event.target.parentNode
-        parent.style.display = 'none'
+function renderClosePosts() {
+    let postButtonClose = document.querySelectorAll('.post__button-close')
+
+    postButtonClose.forEach(item => {
+        item.addEventListener('click', event => {
+            let parent = event.target.parentNode
+            parent.style.display = 'none'
+        })
     })
-})
+}
